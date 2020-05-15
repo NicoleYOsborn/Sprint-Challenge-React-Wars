e3aa8a0aef12dbe5bce94841092514713a5628fb
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-05-15 11:00:25 -0700

    characters rendering

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
-
 import './App.css';
-import Characters from './components/Character';
+import Character from './components/Character';
 import axios from 'axios';
+import styled from 'styled-components'
 
 
 const App = () => {
@@ -13,20 +13,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [data, setData]  = useState('')
+  const [chars, setChars]  = useState([])
   useEffect(()=>{
     axios
     .get('https://swapi.dev/api/people/')
     .then(result =>{
-      setData(result.data)
-      console.log(result)
+      setChars(result.data.results)
+      console.log(result.data.results)
     })
+    console.log (chars)
   }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {chars.map(x => <Character chars = {x}/> )}
+        
+     
+      
     </div>
+    
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,14 +2,48 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const CharacterCard = styled.div``
+const CharacterCard = styled.div`
+background: lightgray;
+width: 50%;
+margin: 4%;
+padding: 2%;
+box-sizing: border-box;
+border: 1 Labelx solid black;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+`
+const Name = styled.h2`
+display: flex;
+width: 100%;
+justify-content: center;
+`
 
-const Character = props =>{
-
+const Label = styled.p`
+font-weight: bold;
+`
+const Character = (props) =>{
+    // console.log(props.chars[0]);
+    // const Labelerson = props.chars[0];
+    // console.log(Labelerson);
     return(
 
         <CharacterCard>
-            <h2>{props[0].name}</h2>
+            <Name>{props.chars.name}</Name>
+          
+            <Label>Birth Year: </Label>
+            <p>{props.chars.birth_year} </p>
+            <Label>Gender: </Label>
+            <p>{props.chars.gender}</p>
+            <Label>Eye color: </Label>
+            <p>{props.chars.eye_color}</p>
+            <Label>Hair color: </Label>
+            <p>{props.chars.hair_color}</p>
+            <Label>Height: </Label>
+            <p>{props.chars.height}</p>
+            <Label>Mass: </Label>
+            <p>{props.chars.mass}</p>
+            
 
         </CharacterCard>
     )
