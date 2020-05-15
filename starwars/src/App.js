f21d386c7d0ee3cf55719e44571602f1398a5741
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-05-15 09:09:28 -0700

    questions answered

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
+import Characters from './components/Character';
+import axios from 'axios';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +12,16 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [data, setData]  = useState('')
+  useEffect(()=>{
+    axios
+    .get('https://swapi.dev/api/people/')
+    .then(result =>{
+      setData(result.data)
+      console.log(result)
+    })
+  }, [])
 
   return (
     <div className="App">
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+const CharacterCard = styled.div``
+
+const Character = props =>{
+
+    return(
+
+        <CharacterCard>
+            <h2>{props[0].name}</h2>
+
+        </CharacterCard>
+    )
+}
+
+export default Character;
